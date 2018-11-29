fd41fb5c60641197d7edd518feefdc7a78f55c07
Kevin Kamto <kamtokevin@gmail.com>
2018-11-29 14:45:06 -0500

    fix(nextcloud): removed nextcloud directory from soure control

--- nextcloud/apps/apporder/l10n/eu.js
+++
@@ -1,9 +0,0 @@
-OC.L10N.register(
-    "apporder",
-    {
-    "AppOrder" : "AppAgindua",
-    "App Order" : "AppAgindua",
-    "Set a default order for all users. This will be ignored, if the user has setup a custom order." : "Defektuzko agindu bat definitu erabiltzaile guztientzat. Erabiltzaileak agindu berezia badu, honi ezikusi egingo zaio.",
-    "Drag the app icons to change their order." : "App-aren ikonoak arrastatu ordena aldatzeko."
-},
-"nplurals=2; plural=(n != 1);");
